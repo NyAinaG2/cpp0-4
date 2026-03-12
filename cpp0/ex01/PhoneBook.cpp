f560f93101d41398770aa099388483d7b20a5a69
Andry Ny Aina Randriamanga <andrrand@c1r5s8.42antananarivo.mg>
2026-03-12 16:18:48 +0300

    edit phonebookcpp

--- cpp0/ex01/PhoneBook.cpp
+++ cpp0/ex01/PhoneBook.cpp
@@ -19,26 +19,20 @@ void	PhoneBook::search(void)
 	std::string	buff;
 	std::cout << std::setw(COL_LEN) << "Index" << "|";
 	for (size_t i = 0; i < FIELD_LEN - 2; i++)
-	{
-		std::cout << std::setw(COL_LEN) << putstr_inrow(g_fields[i]);
-		if (i != FIELD_LEN - 3)
-			std::cout  << "|";
-	}
+		std::cout << std::setw(COL_LEN) << putstr_inrow(g_fields[i]) << (i != FIELD_LEN - 3 ? "|" : "");
 	std::cout  << std::endl;
-	for (int i = 0; i < CONTACTS_LEN; i++)
+	for (int i = 0; i < CONTACTS_LEN && !this->_contacts[i]._infos[0].empty(); i++)
 	{
 		std::cout << std::setw(COL_LEN) << i + 1 << "|";
 		for (int j = 0; j < FIELD_LEN - 2; j++)
-		{
-			std::cout << std::setw(COL_LEN) << putstr_inrow(this->_contacts[i]._infos[j]);
-			if (j != FIELD_LEN - 3)
-				std::cout  << "|";
-		}
+			std::cout << std::setw(COL_LEN) << putstr_inrow(this->_contacts[i]._infos[j]) << (j != FIELD_LEN - 3 ? "|" : "");
 		std::cout <<  std::endl;
 	}
 	std::cout << "Choose a index for more details >";
 	if (!std::getline(std::cin, buff) || std::cin.eof())
 		std::exit(0);
+	if (buff.empty())
+		return ;
 	if(isNumber(buff) && std::atoi(buff.c_str()) >= 1 && std::atoi(buff.c_str()) <= CONTACTS_LEN)
 		this->_contacts[std::atoi(buff.c_str()) - 1].display_infos();
 	else

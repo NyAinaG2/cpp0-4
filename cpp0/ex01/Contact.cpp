31eb436eb4bd01e77b85e89afc6bcb00a38acd6d
Andry Ny Aina Randriamanga <andrrand@c1r5s5.42antananarivo.mg>
2026-03-13 17:34:49 +0300

    correct Contact

--- cpp0/ex01/Contact.cpp
+++ cpp0/ex01/Contact.cpp
@@ -7,52 +7,80 @@ Contact::~Contact(void) {}
 
 Contact::Contact(const Contact &source)
 {
-	for (int i = 0; i < FIELD_LEN; i++)
-		this->_infos[i] = source._infos[i];
+	this->_first_name = source._first_name;
+	this->_last_name = source._last_name;
+	this->_nickname = source._nickname;
+	this->_phone_number = source._phone_number;
+	this->_darkest_secret = source._darkest_secret;
 }
 
-bool	Contact::set_infos(void)
+bool Contact::empty(void) const
 {
-	Contact	tmp;
+	return _first_name.empty();
+}
+
+bool Contact::set_infos(void)
+{
+	Contact tmp;
 	for (size_t i = 0; i < FIELD_LEN; i++)
 	{
-		std::cout << "(ADD) "<< g_fields[i] << ">";
-		if (!std::getline(std::cin, tmp._infos[i]) || std::cin.eof())
+		std::cout << "(ADD) " << g_fields[i] << ">";
+		std::string line;
+		if (!std::getline(std::cin, line) || std::cin.eof())
 			std::exit(0);
-		if (tmp._infos[i].empty() || isNotPrintable(tmp._infos[i]))
+		if (line.empty() || isNotPrintable(line))
 		{
 			std::cout << "Invalid input" << std::endl;
-			return (false);
+			return false;
 		}
-		if (i == 3 && !isNumber(tmp._infos[i]))
+		if (i == 3 && !isNumber(line))
 		{
 			std::cout << "Invalid phone number" << std::endl;
-			return (false);
+			return false;
+		}
+		switch (i)
+		{
+			case 0:
+				tmp._first_name = line;
+				break;
+			case 1:
+				tmp._last_name = line;
+				break;
+			case 2:
+				tmp._nickname = line;
+				break;
+			case 3:
+				tmp._phone_number = line;
+				break;
+			case 4:
+				tmp._darkest_secret = line;
+				break;
 		}
 	}
 	*this = tmp;
-	return (true);
+	return true;
 }
 
-void	Contact::display_in_row(int index)
+void Contact::display_in_row(int index) const
 {
 	std::cout << std::setw(10) << index + 1 << "|";
-	for (int j = 0; j < FIELD_LEN - 2; j++)
-		std::cout << std::setw(10) << putstr_inrow(this->_infos[j]) << (j != FIELD_LEN - 3 ? "|" : "");
-	std::cout <<  std::endl;
+	std::cout << std::setw(10) << putstr_inrow(_first_name) << "|";
+	std::cout << std::setw(10) << putstr_inrow(_last_name) << "|";
+	std::cout << std::setw(10) << putstr_inrow(_nickname) << std::endl;
 }
 
-void	Contact::display_detailed_infos(void)
+void Contact::display_detailed_infos(void) const
 {
-	if (this->_infos[0].empty())
+	if (empty())
 	{
 		std::cout << "Contact not found" << std::endl;
-		return ;
+		return;
 	}
-	for (size_t i = 0; i < FIELD_LEN; i++)
-		std::cout << g_fields[i] << " : "<< this->_infos[i] << std::endl;
+	std::cout << std::setw(15) << g_fields[0] << ": " << _first_name << std::endl;
+	std::cout << std::setw(15) << g_fields[1] << ": " << _last_name << std::endl;
+	std::cout << std::setw(15) << g_fields[2] << ": " << _nickname << std::endl;
+	std::cout << std::setw(15) << g_fields[3] << ": " << _phone_number << std::endl;
+	std::cout << std::setw(15) << g_fields[4] << ": " << _darkest_secret << std::endl;
 }
-bool	Contact::empty(void) const
-{
-	return this->_infos[0].empty();
-}
+
+
--- cpp0/ex01/Contact.hpp
+++ cpp0/ex01/Contact.hpp
@@ -5,14 +5,19 @@
 class Contact
 {
 	private:
-		std::string _infos[FIELD_LEN];
+		std::string _first_name;
+		std::string _last_name;
+		std::string _nickname;
+		std::string _phone_number;
+		std::string _darkest_secret;
+
 	public:
 		Contact(void);
 		Contact(const Contact &source);
 		~Contact(void);
-		bool	set_infos(void);
-		void	display_in_row(int index);
-		void	display_detailed_infos(void);
 		bool	empty(void) const;
+		bool	set_infos(void);
+		void	display_in_row(int index) const;
+		void	display_detailed_infos(void) const;
 };
 #endif

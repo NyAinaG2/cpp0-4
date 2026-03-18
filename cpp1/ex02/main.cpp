6a52ac76b9b75b5ff2c8b035dc21ce3c291d7c3c
Andry Ny Aina Randriamanga <andrrand@c1r4s9.42antananarivo.mg>
2026-03-18 15:55:34 +0300

    correct ex02 main

--- cpp1/ex02/main.cpp
+++ cpp1/ex02/main.cpp
@@ -7,13 +7,10 @@ int	main(void)
 	std::string& stringREF = str;
 
 	std::cout << "The memory address of the string variable: " << &str << std::endl;
-	std::cout << "The memory address of the stringPTR: " << &stringPTR << std::endl;
-	std::cout << "The memory address of the stringREF: " << &stringREF << std::endl;
-	std::cout << "Display the address by using stringPTR: " << stringPTR <<std::endl;
-	std::cout << "Display the address by using stringREF: " << &stringREF <<std::endl;
-	std::cout << "The value pointed to by stringPTR: " << stringPTR << std::endl;
-	std::cout << "Display the value by using the variable: " << str <<std::endl;
-	std::cout << "Display the value by using stringPTR: " << *stringPTR <<std::endl;
-	std::cout << "Display the value by using stringREF: " << stringREF <<std::endl;
+	std::cout << "The memory address held by stringPTR: " << stringPTR << std::endl;
+	std::cout << "The memory address held by stringREF: " << &stringREF << std::endl << std::endl;
+	std::cout << "The value of the string variable: " << str <<std::endl;
+	std::cout << "The value pointed to by stringPTR: " << *stringPTR <<std::endl;
+	std::cout << "The value pointed to by stringREF: " << stringREF <<std::endl;
 	return (0);
 }
